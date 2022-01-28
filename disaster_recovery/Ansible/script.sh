a8661deb58882189726f6217ae109b282c38edf1
td <dmytro.trendiuk@dev.pro>
2022-01-28 09:25:22 +0200

    Optimize Ansible roles with dynamic inventory

--- disaster_recovery/Ansible/script.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-cd /home/ec2-user/ansible/disaster_recovery/Ansible;
-ansible-galaxy collection install amazon.aws;
-pip3 install --user boto3;
-database_private_ip=$(ansible _database -i inventory_aws_ec2.yml -m shell -a 'hostname -I' | sed -n '2p')
-echo database_IP: $database_private_ip >> roles/phpmyadmin/vars/main.yml;
-ansible-playbook -i inventory_aws_ec2.yml playbook.yml --vault-password-file /home/ec2-user/vault_pass.txt
